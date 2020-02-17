e1fb907c081d1990bc5348450d838317ce7a088f
Marco <marco.gzz1405@gmail.com>
2020-02-17 17:46:33 -0600

    add manu navigation

--- app/src/main/java/com/example/appwhere/activities/MainActivity.kt
+++ app/src/main/java/com/example/appwhere/activities/MainActivity.kt
@@ -5,17 +5,36 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.appwhere.R
+import com.example.appwhere.bases.BaseActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.navigation_map -> {
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_legend -> {
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_legend -> {
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
 
     companion object {
         fun getInstance(context: Context): Intent {
             return Intent(context, MainActivity::class.java)
         }
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
     }
 }
